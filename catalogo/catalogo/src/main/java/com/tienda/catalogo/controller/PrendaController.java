31bb4ea16ac2ad5a392fa82166d1ed6bb2b5e226
nsgrvce99 <ni.sepulvedaz@duocuc.cl>
2026-06-17 17:38:00 -0400

    Lota swagger funcional en todos los microservices broden

--- catalogo/catalogo/src/main/java/com/tienda/catalogo/controller/PrendaController.java
+++ catalogo/catalogo/src/main/java/com/tienda/catalogo/controller/PrendaController.java
@@ -3,6 +3,11 @@ package com.tienda.catalogo.controller;
 import com.tienda.catalogo.dto.PrendaSimpleDTO;
 import com.tienda.catalogo.model.Prenda;
 import com.tienda.catalogo.service.PrendaService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -16,20 +21,8 @@ import java.util.Map;
 @RequestMapping("/prendas")
 public class PrendaController {
     private final PrendaService service;
-////////////////////////////////////////////////
     
-    @Operation( summary = "Obtener prenda por ID",
-                    description = "Busca una prenda en el catálogo por su ID")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Prenda encontrada"),
-        @ApiResponse(responseCode = "404", description = "Prenda no encontrada"),
-        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
-    })
-        @GetMapping
-        public ResponseEntity<List<Prenda>> listarPrendas() {
-            return ResponseEntity.ok(service.listarCatalogo());
-        }
-        @Operation( summary = "Crear una nueva prenda",
+    @Operation( summary = "Crear una nueva prenda",
                     description = "Agrega una nueva prenda al catálogo"
     )
     @ApiResponses(value = {
@@ -111,61 +104,12 @@ public class PrendaController {
         if(p != null) return ResponseEntity.ok(p);
         return ResponseEntity.notFound().build();
     }     
-/////////////////////////////////////////////
     public PrendaController(PrendaService service) {
         this.service = service;
     }
 
-    @PostMapping("/agregar")
-    public ResponseEntity<Prenda> crearPrenda(@Valid @RequestBody Prenda prenda) {
-        return ResponseEntity.status(201).body(service.guardarPrenda(prenda));
-        
     }
 
-    @GetMapping("/listar-dto")
-    public ResponseEntity<List<PrendaSimpleDTO>> listarDTO() {
-        return ResponseEntity.ok(service.listarCatalogoSimple());
-    }
-    
-    @GetMapping("/{id}")
-    public ResponseEntity<Prenda> buscarPorId(@PathVariable Integer id) {
-        Prenda p = service.buscarPorId(id);
-        if(p != null) return ResponseEntity.ok(p);
-        return ResponseEntity.notFound().build();
-    }
-    
-    @DeleteMapping("/eliminar/{id}")
-    public ResponseEntity<?> eliminar(@PathVariable Integer id){
-        try {
-            String mensaje = service.eliminar(id);
-            if (mensaje.contains("Prenda no encontrada")) {
-                    return ResponseEntity.status(404).body(mensaje);
-                }
-                    return ResponseEntity.ok(mensaje);
-            } catch (Exception e) {
-
-                    return ResponseEntity.status(500).body("error");
-            }
-    }
-
-    @PutMapping("/actualizar")
-    public ResponseEntity<?> actualizar(@Valid @RequestBody Prenda prenda, BindingResult result) {
-        if (result.hasErrors()) {
-            Map<String, String> errores = new HashMap<>();
-            result.getFieldErrors().forEach(error -> errores.put(error.getField(), error.getDefaultMessage()));
-            return ResponseEntity.badRequest().body(errores);
-        }
-        try {
-            String mensaje = service.actualizar(prenda);
-            if (mensaje.contains("Prenda no encontrada")) {
-                return ResponseEntity.status(404).body(mensaje);
-            }
-            return ResponseEntity.ok(mensaje);
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error");
-        }
-    }
-}
 
     
 
--- catalogo/catalogo/src/main/java/com/tienda/catalogo/swagger/SwaggerConfig.java
+++ catalogo/catalogo/src/main/java/com/tienda/catalogo/swagger/SwaggerConfig.java
@@ -1,8 +1,8 @@
 package com.tienda.catalogo.swagger;
 
-import io.swagger.v3.oas.model.OpenAPI;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.contact;
+import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,18 +11,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Catalogo API")
-                        .version("1.0")
-                        .description("API para gestionar el catálogo de productos"));
-                        .contact(new Contact()
-                                .name("Equipo de Desarrollo")
-                                .email("desarrollo@tienda.com")
-                        )
-                );              
-    }
+        @Bean
+        public OpenAPI catalogoOpenAPI(){
+                return new OpenAPI()
+                        .info(new Info()
+                                .title("API de Catálogo")
+                                .description("API REST para la gestión del catálogo de productos")
+                                .version("3.0")
+                                .contact(new Contact()
+                                        .name("Coreplay")
+                                        .email("coreplay@tienda.com")
+                                )
+                        );
+        }
 
 }
